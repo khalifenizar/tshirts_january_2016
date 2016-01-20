0427e53759fd4e1a0072ab1e7c7ff56a5a06ec4a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-01-20 11:57:31 -0500

    Add items to cart

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
+enable(:sessions)
+
 # 1. list of shirts
 # 2. single shirt page
 # 3. post to add shirt to shopping cart
@@ -15,7 +17,12 @@ require "sinatra/reloader" if development?
 inventory = [ "Deep V T-Shirt", "Ed Hardy T-Shirt", "Cat T-Shirt" ]
 
 get "/" do
+  if session[:cart].nil?
+    session[:cart] = []
+  end
+
   @shirts = inventory
+  @number_of_items = session[:cart].length
 
   erb(:tshirts)
 end
@@ -25,4 +32,18 @@ get "/tshirts/:tshirt_id" do
   @tshirt_type = inventory[@tshirt_id - 1]
 
   erb(:tshirt_page)
+end
+
+post "/add_to_cart" do
+  tshirt_id = params[:tshirt_id]
+
+  # Only set cart to empty array if it's nil
+  # Otherwise it will overwrite stuff I previously had in cart
+  if session[:cart].nil?
+    session[:cart] = []
+  end
+
+  session[:cart].push(tshirt_id)
+
+  redirect to("/")
 end
